ef9c0f2964cba7c49d2ef9f7bc002d27cc8322e6
Stephen <stephengroat@users.noreply.github.com>
2019-03-14 10:49:33 -0700

    add reminder to revert Qt59 change on next release

--- pkgs/applications/science/robotics/apmplanner2/default.nix
+++ pkgs/applications/science/robotics/apmplanner2/default.nix
@@ -5,6 +5,7 @@
 
 stdenv.mkDerivation rec {
   name = "apmplanner2-${version}";
+  # TODO revert pin to Qt59 in pkgs/top-level/all-packages.nix on next release
   version = "2.0.26";
   src = fetchFromGitHub {
     owner = "ArduPilot";
